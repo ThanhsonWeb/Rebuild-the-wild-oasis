ec2416d266f79b2879cb0c18fc5b459dcb279569
ThanhsonWeb <sondeptroi@gmail.com>
2026-05-29 08:48:21 +0700

    Connect Supabse to the app

---
+++ src/services/supabase.js
@@ -0,0 +1,7 @@
+import { creatClient } from "@supabase/supabase-js";
+
+
+const supabaseUrl = "https://cmzzfqlehqfrztjvsxrk.supabase.co";
+const supabaseAnonKey = "sb_publishable_Kr-FxUtoLNnyN7pM23J2kA_Sr_10FOb";
+
+export const supabase = creatClient(supabaseUrl, supabaseAnonKey);
